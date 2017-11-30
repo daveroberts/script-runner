04ffebcc8d105a113820bafc4fdd44915342866e
Dave Roberts <dave.a.roberts@gmail.com>
2017-11-30 12:23:43 -0500

    fix pretty json

--- frontend/app/scripts/Script.vue
+++ frontend/app/scripts/Script.vue
@@ -50,7 +50,7 @@ export default {
     },
      prettyJson: function(){
       var value = this.output
-      let json = JSON.stringify(JSON.parse(value), null, 2)
+      let json = JSON.stringify(value, null, 2)
       json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
       return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+]?\d+)?)/g, function(match) {
         var cls = 'number'

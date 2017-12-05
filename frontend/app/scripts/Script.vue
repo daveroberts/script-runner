c94f6b6cbbd32a31051e039d5239a2a2b857ce53
Dave Roberts <dave.a.roberts@gmail.com>
2017-12-05 16:58:08 -0500

    highlight.js

--- frontend/app/scripts/Script.vue
+++ frontend/app/scripts/Script.vue
@@ -1,83 +1,87 @@
 <template>
   <div>
-    <h1>{{script.name}}</h1>
-    <button @click="toggle_edit()">flipmode</button>
-    <form>
-      <table class="table">
-        <tbody>
-          <tr>
-            <th>Name</th>
-            <td>
-              <input v-if="editing" type="text" v-model="script.name" />
-              <span v-else>{{script.name}}</span>
-            </td>
-          </tr>
-          <tr>
-            <th>Description</th>
-            <td>
-              <input v-if="editing" type="text" v-model="script.description" />
-              <span v-else>{{script.description}}</span>
-            </td>
-          </tr>
-        </tbody>
-        <tbody v-for="trigger in script.triggers">
-          <tr>
-            <th>Type</th>
-            <td>
-              <select v-if="editing" v-model="trigger.type">
-                <option v-for="type in trigger_types" v-bind:value="type">{{type}}</option>
-              </select>
-              <span v-else>{{trigger.type}}</span>
-            </td>
-          </tr>
-          <tr v-if="trigger.type == 'CRON'">
-            <th>Every</th>
-            <td>
-              <span v-if="editing"><input type="text" v-model="trigger.every" /> minutes</span>
-              <span v-else>{{trigger.every}} minutes</span>
-            </td>
-          </tr>
-          <tr v-if="trigger.type == 'QUEUE'">
-            <th>Queue Name</th>
-            <td>
-              <input v-if="editing" type="text" v-model="trigger.queue_name" />
-              <span v-else>{{trigger.queue_name}}</span>
-            </td>
-          </tr>
-        </tbody>
-        <tbody>
-          <tr>
-            <th>Code</th>
-            <td>
-              <div v-if="editing" class="code_editor">
-                <codemirror v-model="script.code" :options="editorOptions"></codemirror>
-              </div>
-              <div v-else>{{script.code}}</div>
-            </td>
-          </tr>
-          <tr>
-            <th></th>
-            <td><button>Save</button></td>
-          </tr>
-        </tbody>
-      </table>
-    </form>
-    <div v-if="!editing">
-      <h2>Last 10 Runs</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Run</th>
-            <th>Status</th>
-          </tr>
-        </thead>
-        <tbody v-for="run in runs">
-          <tr>
-            <td>{{run.run_at}}</td>
-            <td>{{run.output}}</td>
-          </tr>
-        </tbody>
-      </table>
+    <div v-if="script">
+      <h1>{{script.name}}</h1>
+      <button @click="toggle_edit()">flipmode</button>
+      <form>
+        <table class="table">
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <td>
+                <input v-if="editing" type="text" v-model="script.name" />
+                <span v-else>{{script.name}}</span>
+              </td>
+            </tr>
+            <tr>
+              <th>Description</th>
+              <td>
+                <input v-if="editing" type="text" v-model="script.description" />
+                <span v-else>{{script.description}}</span>
+              </td>
+            </tr>
+          </tbody>
+          <tbody v-for="trigger in script.triggers">
+            <tr>
+              <th>Type</th>
+              <td>
+                <select v-if="editing" v-model="trigger.type">
+                  <option v-for="type in trigger_types" v-bind:value="type">{{type}}</option>
+                </select>
+                <span v-else>{{trigger.type}}</span>
+              </td>
+            </tr>
+            <tr v-if="trigger.type == 'CRON'">
+              <th>Every</th>
+              <td>
+                <span v-if="editing"><input type="text" v-model="trigger.every" /> minutes</span>
+                <span v-else>{{trigger.every}} minutes</span>
+              </td>
+            </tr>
+            <tr v-if="trigger.type == 'QUEUE'">
+              <th>Queue Name</th>
+              <td>
+                <input v-if="editing" type="text" v-model="trigger.queue_name" />
+                <span v-else>{{trigger.queue_name}}</span>
+              </td>
+            </tr>
+          </tbody>
+          <tbody>
+            <tr>
+              <th>Code</th>
+              <td>
+                <div v-if="editing" class="code_editor">
+                  <codemirror v-model="script.code" :options="editorOptions"></codemirror>
+                </div>
+                <div v-else>
+                  <pre v-highlightjs="script.code"><code class="javascript"></code></pre>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <th></th>
+              <td><button>Save</button></td>
+            </tr>
+          </tbody>
+        </table>
+      </form>
+      <div v-if="!editing">
+        <h2>Last 10 Runs</h2>
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Run</th>
+              <th>Output</th>
+            </tr>
+          </thead>
+          <tbody v-for="run in runs">
+            <tr>
+              <td>{{run.run_at}}</td>
+              <td>{{run.output}}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </template>
@@ -90,7 +94,7 @@ export default {
       state: state,
       editing: false,
       trigger_types: ['CRON', 'QUEUE'],
-       editorOptions: {
+      editorOptions: {
         tabSize: 2,
         mode: 'text/javascript',
         lineNumbers: true,

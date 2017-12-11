6422ddc467dd8b30595960230c402a874f08e581
Dave Roberts <dave.a.roberts@gmail.com>
2017-12-10 20:50:42 -0500

    more extensions

--- TODO.md
+++ TODO.md
@@ -1,17 +1,14 @@
-web interface
-more examples
+system function uuid
 show data flow diagram; show problems solved; show commands can be complex, not primitive; one section of pipeline can be complex, etc.; vs S3, queues can be dynamically named, data sets dynamically created.  LocalDataStorage vs AwsDataStorage
 doesn't need to use custom script, could be any language with eval or scripting capability
-sending emails, tweets
-if script contains input(), warn if no input
+http triggers / http get data
 pretty print dates
 triggers on other data types
   manually "dequeing" other data types
-http triggers / http get data
-call func from func
-system function uuid
+sending emails, tweets
 pull off queue manually
 sftp interface
 threading
 move js modules out of bundle
 set_store: make it return false on duplicate
+if script contains input(), warn if no input
---
+++ backend/app/extensions/sftp.rb
@@ -0,0 +1,46 @@
+require 'net/sftp'
+require 'securerandom'
+
+class Sftp
+  def self.icon
+    "fa-folder-open-o"
+  end
+
+  def initialize
+    @sftp = nil
+  end
+
+  # Establish a connection to an SFTP server
+  def sftp_connect(hostname, username, password)
+    @sftp = Net::SFTP.start(hostname, username, :password => password)
+  end
+
+  # Disconnect from an SFTP server
+  def sftp_disconnect(hostname, username, password)
+    @sftp = nil
+  end
+
+  # array of files on the remote path
+  def sftp_dir(remote_path)
+    @sftp.dir.foreach(remote_path)
+  end
+
+  # download a file from the remote path
+  def sftp_download(remote_path)
+    file = @sftp.file.open(remote_path)
+    content = file.gets
+    file.close
+    return content
+  end
+
+  # Move a file on the remote server. TODO: Not yet implemented
+  def sftp_move(remote_path_from, remote_path_to)
+    return false
+  end
+
+  # Delete a file on the remote server. TODO: Not yet implemented
+  def sftp_delete(remote_path)
+    return false
+  end
+
+end

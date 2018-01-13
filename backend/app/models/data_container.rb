2bdeb93f5eb5b59712fb27f38ff0cea87efb44e6
Dave Roberts <dave.a.roberts@gmail.com>
2018-01-12 22:41:56 -0500

    Getting a bit farther with data values

--- backend/app/models/data_container.rb
+++ backend/app/models/data_container.rb
@@ -1,9 +1,28 @@
+require 'securerandom'
+
 # manages data items in database
 class DataContainer
-  $data = {}
-  def initialize(collection, data)
-    @collection = collection
-    @values = data
+  DATA_CONTAINER_COLUMNS = [:id, :collection_id, :collection, :name, :image_id, :created_at]
+  DATA_VALUE_COLUMNS = [:id, :data_container_id, :key, :type, :array, :summary, :value, :linked_image_id, :linked_collection, :linked_collection_id, :linked_queue_name, :linked_tag, :novel, :filedata, :created_at]
+  attr_reader :collection, :name
+  def initialize(options = {
+      id: nil,
+      collection: nil,
+      collection_id: nil,
+      values: nil,
+      name: nil,
+      image_id: nil,
+      created_at: nil
+  })
+    @values = {}
+    set_all(options[:values])
+    @id = options[:id] if options[:id]
+    @collection_id = options[:collection_id] if options[:collection_id]
+    @collection = options[:collection] if options[:collection]
+    @name = options[:name] if options[:name]
+    @image_id = options[:image_id] if options[:image_id]
+    @created_at = options[:created_at] if options[:created_at]
+    raise "Must specify a collection" if !@collection
   end
 
   def [](key)
@@ -13,10 +32,52 @@ class DataContainer
   end
 
   def []=(key, value)
-    @values[key] = value
+    set(key, value)
   end
 
   def save()
+    @collection_id = SecureRandom.uuid if !@collection_id
+    # Save or update the container
+    if !@id
+      @id = SecureRandom.uuid
+      @created_at = Time.now
+      @name = @id if !@name
+      DataMapper.insert('data_containers', fields)
+    else
+      DataMapper.update('data_containers', fields)
+    end
+    @values.each do |key, value|
+      #TODO: Save to separate fields
+      # Get the existing value
+      sql = "
+SELECT `id`, `value` FROM data_values WHERE data_container_id=? AND `key`=?
+"
+      data = DataMapper.raw_select(sql, [@id, key.to_s])
+      if data.length == 0
+        DataMapper.insert('data_values', {
+          id: SecureRandom.uuid,
+          data_container_id: @id,
+          key: key.to_s,
+          type: :string,
+          array: false,
+          summary: value,
+          value: value,
+          created_at: Time.now
+        })
+      else
+        if data[0][:value] == value
+          # ignore this for now
+        else
+          sql = "
+UPDATE data_values SET value = ? WHERE `key` = ? AND data_container_id = ?
+"
+          DataMapper.raw_select(sql, [value, key.to_s, @id])
+        end
+      end
+      # TODO: tombstone and insert
+      # TODO: support for arrays
+      # TODO: support for other data types
+    end
     self
   end
 
@@ -24,20 +85,56 @@ class DataContainer
     @values.to_json(opts)
   end
 
-  def self.save(collection, data)
-    dc = DataContainer.new(collection, data)
-    $data[collection] = [] if !$data.has_key?(collection)
-    $data[collection].push(dc)
-    return dc
+  def self.save(collection, values)
+    dc = DataContainer.new({collection: collection, values: values})
+    dc.save
   end
 
   def self.find(collection, criteria)
-    return nil if !$data.has_key?(collection)
-    $data[collection].detect do |item|
-      criteria.each do |k,v|
-        return false if item[k] != v
-      end
-      true
+    if criteria.class == String
+      sql = "
+SELECT
+  dc.id as dc_id,
+  dc.name as dc_name,
+  dc.image_id as dc_image_id,
+  dc.created_at as dc_created_at,
+  dv.id as dv_id,
+  dv.`key` as dv_key,
+  dv.`type` as dv_type,
+  dv.value as dv_value,
+  dv.created_at as dv_created_at
+FROM data_containers dc
+  LEFT JOIN data_values dv ON dc.id = dv.data_container_id
+WHERE
+  dv.`key` = ?
+"
+      data = DataMapper.select(sql, {prefix: "dc", has_many: [
+        { prefix: 'dc' }
+      ]}, criteria)
+      binding.pry
+    else
+      sql = "
+SELECT
+  dc.id as dc_id,
+  dc.name as dc_name,
+  dc.image_id as dc_image_id,
+  dc.created_at as dc_created_at,
+  dv.id as dv_id,
+  dv.`key` as dv_key,
+  dv.`type` as dv_type,
+  dv.value as dv_value,
+  dv.created_at as dv_created_at
+FROM data_containers dc
+  LEFT JOIN data_values dv ON dc.id = dv.data_container_id
+  #{criteria.each_with_index.map{|c,i|"LEFT JOIN data_values d#{i} ON dc.id=d#{i}.data_container_id"}.join("\n")}
+WHERE
+  dc.collection = ? AND
+  #{criteria.each_with_index.map{|(k,v),i|"d#{i}.`key`=? AND d#{i}.value=?"}.join(" AND \n")}
+"
+      data = DataMapper.select(sql, {prefix: 'dc', has_many: [
+        { data_values: { prefix: 'dv' } }
+      ] },[collection, criteria.to_a.flatten.map{|item|item.to_s}].flatten)
+      binding.pry
     end
   end
 
@@ -50,5 +147,34 @@ class DataContainer
       true
     end
     items
+  end
+
+  private
+  
+  def set_all(hash)
+    return if !hash
+    hash.each do |key, value|
+      set(key, value)
+    end
+  end
+
+  def set(key, value)
+    @name = value if key.to_s.casecmp("label")==0 && !@name
+    @name = value if key.to_s.casecmp("name")==0 && !@name
+    @name = value if key.to_s.casecmp("title")==0 && !@name
+    @name = value if key.to_s.casecmp("subject")==0 && !@name
+    @name = value if key.to_s.casecmp("summary")==0 && !@name
+    @values[key] = value
+  end
+
+  def fields
+    return {
+      id: @id,
+      collection_id: @collection_id,
+      collection: @collection,
+      name: @name,
+      image_id: @image_id,
+      created_at: @created_at
+    }
   end
 end
